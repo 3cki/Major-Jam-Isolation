6735452e99b2842e23202f20809fac6cee51233a
3cki <eckertm2000@gmail.com>
2020-03-25 23:43:38 +0100

    Initial commit

---
+++ Major Jam Isolation/Assets/Scripts/Must All Scenes/ResolutionSetter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionSetter : MonoBehaviour
+{
+    void Start()
+    {
+        int width = 320; // or something else
+        int height = 240; // or something else
+        bool isFullScreen = true; // should be windowed to run in arbitrary resolution
+        int desiredFPS = 60; // or something else
+
+        Screen.SetResolution(width, height, isFullScreen, desiredFPS);
+    }
+}
---
+++ Major Jam Isolation/Assets/Scripts/Room/Lightswitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightswitch : MonoBehaviour
+{
+    public GameObject lightswitch;
+
+    void Start()
+    {
+        lightswitch.transform.localRotation = new Quaternion(transform.localRotation.x, transform.localRotation.y, transform.localRotation.z, transform.localRotation.w);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
